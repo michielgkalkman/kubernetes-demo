363e17d6cdddf214f824ae16b07246750de22322
Michiel Kalkman <michiel.kalkman@atos.net>
2020-11-06 21:46:20 +0100

    initial commit

---
+++ kubernetes-demo/src/main/java/org/taHja/wo/kubernetesdemo/KubernetesDemoApplication.java
@@ -0,0 +1,11 @@
+package org.taHja.wo.kubernetesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesDemoApplication.class, args);
+	}
+}
